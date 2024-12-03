39c4623c842b1283d8c2d89dd1e87ae00bfc37ab
Stephen321 <Stephen321@users.noreply.github.com>
2024-12-03 10:28:04 +0000

    day2 part 1

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -1,24 +1,45 @@
+use std::fs;
+
 fn main() {
-    let lines: Vec<&str> = vec![
-        "7 6 4 2 1",
-        "1 2 7 8 9",
-        "9 7 6 2 1",
-        "1 3 2 4 5",
-        "8 6 4 4 1",
-        "1 3 6 7 9",
-    ];
-    for line in lines {
-        let levels: Vec<u32> = line
+    let input_file_path = "./src/day2/input.txt";
+    let contents = fs::read_to_string(input_file_path);
+
+    let mut safe_reports = 0u32;
+    for line in contents.unwrap().lines() {
+        let levels: Vec<i32> = line
             .split_whitespace()
-            .map(str::parse::<u32>)
+            .map(str::parse)
             .map(Result::unwrap)
             .collect();
         let increasing = levels[1] > levels[0];
+        let mut safe = true;
         for (index, level) in levels.iter().enumerate() {
             if index == 0 {
                 continue;
             }
+            let diff = level - levels[index - 1];
+            if increasing {
+                if diff.is_negative() {
+                    safe = false;
+                    break;
+                }
+            } else {
+                if diff.is_positive() {
+                    safe = false;
+                    break;
+                }
+            }
+            match diff.abs() {
+                1 | 2 | 3 => continue,
+                _ => {
+                    safe = false;
+                    break;
+                }
+            }
         }
-        println!("{:?}", levels);
+        if safe {
+            safe_reports += 1;
+        }
     }
+    println!("{:?}", safe_reports);
 }
