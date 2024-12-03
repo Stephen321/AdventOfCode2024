786b6bb4c34abb6cfee87ee1a10d432383ca5191
Stephen321 <Stephen321@users.noreply.github.com>
2024-12-03 00:22:09 +0000

    started attempt at day2 part 1

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -1,1 +1,24 @@
-fn main() {}
+fn main() {
+    let lines: Vec<&str> = vec![
+        "7 6 4 2 1",
+        "1 2 7 8 9",
+        "9 7 6 2 1",
+        "1 3 2 4 5",
+        "8 6 4 4 1",
+        "1 3 6 7 9",
+    ];
+    for line in lines {
+        let levels: Vec<u32> = line
+            .split_whitespace()
+            .map(str::parse::<u32>)
+            .map(Result::unwrap)
+            .collect();
+        let increasing = levels[1] > levels[0];
+        for (index, level) in levels.iter().enumerate() {
+            if index == 0 {
+                continue;
+            }
+        }
+        println!("{:?}", levels);
+    }
+}
